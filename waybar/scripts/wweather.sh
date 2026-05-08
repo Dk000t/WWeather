0e0999a9c5357ea22a0eef9689922c0a36fade57
Dk000t <92294163+Dk000t@users.noreply.github.com>
2026-05-08 16:45:03 +0200

    Enhance wweather.sh with region and temperature unit
    Updated weather script to include temperature unit and region.

--- waybar/scripts/wweather.sh
+++ waybar/scripts/wweather.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-#Avoid to load partial output on waybar at startup.
+# Prevents the script from failing when the connection is not yet established
 while ! ping -c1 api.open-meteo.com &>/dev/null; do
     sleep 1
 done
 
-#Set city and province.
+# Set city, region and temperature unit
 city="Brusciano"
-province="Naples"
+region="Campania"
+unit=celsius
 
-#Retrieve geodata from the chosen city and province.
+# Retrieve geodata
 geodata=$(curl -s "https://geocoding-api.open-meteo.com/v1/search?name=${city}" \
-| jq ".results[] | select(.admin2 == \"${province}\")")
+| jq ".results[] | select(.admin1 == \"${region}\")")
 
-#Extract latitude from geodata.
+# Extract latitude
 latitude=$(echo "$geodata" | jq -r '.latitude')
 
-#Extract longitude from geodata.
+# Extract longitude
 longitude=$(echo "$geodata" | jq -r '.longitude')
 
-#Retrieve the weather for a given latitude and longitude.
-weather=$(curl -s "https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true")
+# Retrieve the weather
+weather=$(curl -s "https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&temperature_unit=${unit}")
 
-#Extract temperature.
+# Extract temperature
 temperature=$(echo "$weather" | jq -r '.current_weather.temperature | floor')
 
-#Extract weathercode.
+# Extract temperature unit symbol
+unit_text=$(echo "$weather" | jq -r '.current_weather_units.temperature')
+
+# Extract weathercode
 icon=$(echo "$weather" | jq -r '.current_weather.weathercode')
 
-#Extract day/night indicator.
+# Extract day/night indicator
 is_day=$(echo "$weather" | jq -r '.current_weather.is_day')
 
-#Convert weathercode to icon.
+# Convert weathercode to icon
 if [[ "$icon" -eq 0 ]]; then
   if [[ "$is_day" -eq 1 ]]; then
     icon="☀️"
@@ -40,12 +44,12 @@ if [[ "$icon" -eq 0 ]]; then
   fi
 else
   case $icon in
-    1|2|3|45)                     			icon="☁️" ;;  # Cloud
+    1|2|3|45)                     		icon="☁️" ;;  # Cloud
     71|73|75|77|85|86)         			icon="🌨️";;   # Snow
     51|53|55|56|57|61|63|65|66|67|80|81|82) 	icon="🌧️";;   # Rain
     95|96|99)                  			icon="⛈️";;   # Thunderstorm
   esac
 fi
 
-#Output the icon and temperature.
-echo "${icon}  ${temperature}°C"
+# Output the icon, temperature and temperature unit
+echo "  ${icon}  ${temperature} ${unit_text}"
